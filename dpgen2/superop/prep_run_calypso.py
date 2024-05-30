b9ea14e571bc357b845ba0604eef7271ba297c8d
Zhenyu Wang <wzy@calypso.cn>
2024-05-30 15:11:06 +0800

    fix: add slice config into slice

--- dpgen2/superop/prep_run_calypso.py
+++ dpgen2/superop/prep_run_calypso.py
@@ -182,10 +182,13 @@ def _prep_run_caly(
     if expl_mode == "default":
         caly_evo_step_config = prep_config
         caly_evo_step_executor = None
+        caly_evo_step_slice_config = deepcopy(template_slice_config)
+        caly_evo_step_slice_config.pop("model_devi_group_size", None)
         template = caly_evo_step_op
     elif expl_mode == "merge":
         caly_evo_step_config = run_config
         caly_evo_step_executor = run_executor
+        caly_evo_step_slice_config = {}
         template = PythonOPTemplate(
             caly_evo_step_op,  # type: ignore
             python_packages=upload_python_packages,
@@ -210,6 +213,7 @@ def _prep_run_caly(
                 "caly_check_opt_file",
             ],
             output_artifact=["traj_results"],
+            **caly_evo_step_slice_config,
         ),
         parameters={
             "block_id": prep_run_caly_steps.inputs.parameters["block_id"],
